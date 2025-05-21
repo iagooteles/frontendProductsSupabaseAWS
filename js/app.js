02b84bb1c181965f872f7cef9c71777416fbe641
iagooteles <iagooteles@gmail.com>
2025-05-21 18:48:34 -0300

    chagend styles

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const updateProductDescription = document.querySelector('#update-description');
 
 const updateModal = new bootstrap.Modal(document.getElementById('updateModal'));
 
-const route = "http://18.218.72.151:3000";
+const route = "http://18.224.15.132:3000";
 
 async function fetchProducts() {
   const response = await fetch(route + '/products');
